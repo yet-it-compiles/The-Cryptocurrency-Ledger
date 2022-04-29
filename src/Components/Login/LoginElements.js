9c9144dfcda865ec6d69858e1e6f4876d863b071
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-04-28 17:53:38 -0700

    Removed obsolete modules

--- src/Components/Login/LoginElements.js
+++ src/Components/Login/LoginElements.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 // import {Link} from "react-router-dom";
-import ethImg from "../../../Assets/Images/Vector.png";
+import ethImg from "../../Assets/Images/Vector.png";
 
 export const Container = styled.div`
     position: absolute;
