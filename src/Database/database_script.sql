1cb7aded3964955a0b4c46aeacaf2171b2a51572
cushonz <cushonz@cwu.edu>
2022-05-01 21:24:19 -0700

    created database script to construct MySQL database for other developers

--- src/Database/database_script.sql
+++ src/Database/database_script.sql
@@ -1,69 +1,68 @@
-CREATE TABLE User(
-                     user_id INTEGER PRIMARY KEY,
-                     email varchar[200],
-                     first_name varchar[200],
-                     last_name varchar[200],
-                     username varchar[200],
-                     enrollment_date varchar[10],
-                     premium_user boolean,
-                     phone_number varchar[10]);
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE Holdings(
-                         holding_id INTEGER PRIMARY KEY,
-                         user_id INTEGER,
-                         crypto_symbol varchar[10],
-                         buy_price real,
-                         quantity real,
-                         buy_date varchar[10],
-                         profit_target real,
-                         FOREIGN KEY (user_id) REFERENCES User(user_id),
-                         FOREIGN KEY (crypto_symbol) REFERENCES Crypto(crypto_symbol)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE Crypto(
-                       crypto_symbol varchar[10] PRIMARY KEY,
-                       price real
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
-INSERT INTO User(email,first_name,last_name,username,enrollment_date,premium_user)
-VALUES
-    ('BmxFreestyle@gmail.com','Tyler','Unsworth','BmxFreestyle','04-27-2022',1),
-    ('Zachary.Cushon@gmail.com','Zach','Cushon','HiImZach','04-26-2022',0),
-    ('Lena.WebDev@gmail.com','Lena','Webdev','LenaTheDev','03-27-2022',1),
-    ('Embert@gmail.com','Embert','theMan','Verspar','06-12-2021',1);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
 
-INSERT INTO Crypto(crypto_symbol, price)
-VALUES
-    ('BTC',39150),
-    ('ADA',.8397),
-    ('CRO',.3892),
-    ('MATIC',1.26),
-    ('GALA',0.1691),
-    ('LRC',.8443);
-
-select * from Crypto;
-
-INSERT INTO Holdings(user_id, crypto_symbol, buy_price, quantity,buy_date)
-VALUES
-    (2, 'ADA',.82,50,'04-24-2022'),
-    (2,'BTC',39150,1,'04-24-2022'),
-    (2,'CRO',.40,20.4,'04-24-2022'),
-    (2,'MATIC',1.48,108.56,'04-24-2022'),
-    (1,'CRO',.48,19.56,'04-24-2022'),
-    (2,'ADA',1.28,13.36,'04-24-2022'),
-    (1,'MATIC',1.48,24.56,'04-24-2022'),
-    (2,'MATIC',1.4,108.56,'04-20-2022'),
-    (4,'LRC',.88,10.56,'02-14-2022'),
-    (5,'CRO',.48,108.56,'04-24-2022'),
-    (4,'BTC',115,108.56,'01-14-2012'),
-    (3,'ADA',1.2,108.56,'12-24-2021');
-
-select * from Holdings
+-- -----------------------------------------------------
+-- Table `mydb`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+  `idUser` INT NOT NULL,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `username` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `enrollment_date` DATE NULL,
+  `premium_status` TINYINT NULL,
+  `hash` VARCHAR(45) NULL,
+  PRIMARY KEY (`idUser`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+COMMENT = '\n';
 
 
+-- -----------------------------------------------------
+-- Table `mydb`.`Holdings`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Holdings` (
+  `idHoldings` INT NOT NULL,
+  `idUser` INT NULL,
+  `symbol` VARCHAR(5) NULL,
+  `buy_price` REAL NULL,
+  `target_price` REAL NULL,
+  `trans_date` DATETIME NULL,
+  PRIMARY KEY (`idHoldings`),
+  UNIQUE INDEX `idHoldings_UNIQUE` (`idHoldings` ASC) VISIBLE,
+  INDEX `idUser_idx` (`idUser` ASC) VISIBLE,
+  CONSTRAINT `idUser`
+    FOREIGN KEY (`idUser`)
+    REFERENCES `mydb`.`User` (`idUser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-Drop table Holdings
 
-select first_name,quantity,crypto_symbol,buy_price from Holdings
-INNER JOIN User U on U.user_id = Holdings.user_id
-where first_name = "Embert"
+-- -----------------------------------------------------
+-- Table `mydb`.`Crypto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Crypto` (
+  `symbol` VARCHAR(5) NOT NULL,
+  `price` REAL NULL,
+  PRIMARY KEY (`symbol`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
