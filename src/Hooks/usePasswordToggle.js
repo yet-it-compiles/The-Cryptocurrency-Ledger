4a49f9bde947309256147b42931abe5e5c99e660
mahjestic <lele@cwu.edu>
2022-05-15 12:38:16 -0700

    updated sim-trading icon, and added password toggle file and folder

---
+++ src/Hooks/usePasswordToggle.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+library.add(faEye, faEyeSlash);
+
+const usePasswordToggle = () => {
+ 
+    const [visible, setVisibility] = useState(false);
+  
+    const Icon = (
+        <FontAwesomeIcon 
+            icon= {visible ? "eye-slash" : "eye"} 
+            onClick={ () => setVisibility(visibility => !visibility)} 
+        />
+    );
+    const InputType = visible ? "text" : "password"; 
+  
+    return [InputType, Icon];
+  }
+
+  export default usePasswordToggle;
