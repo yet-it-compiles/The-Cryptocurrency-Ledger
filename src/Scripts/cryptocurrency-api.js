cbb2dccd66ebc2fa500d3985d30b80076961bead
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-05-18 12:40:38 -0700

    Initial commit of API module, also updated package.json to include npm coingecko

---
+++ src/Scripts/cryptocurrency-api.js
@@ -0,0 +1,22 @@
+/**
+ * This module retrieves the requested cryptocurrency attribute information from the API
+ */
+
+class CryptocurrencyApi {
+    constructor(cryptocurrencyName) {
+        this.nameOfCrypto = cryptocurrencyName.toLowerCase();
+    }
+
+    /**
+     *
+     */
+    getCoinInformation() {
+
+    }
+
+
+
+
+
+
+}
