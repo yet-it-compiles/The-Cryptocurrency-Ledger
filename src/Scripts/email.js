09f1297f21ab63cfd1a01dd5f7c1e41b07f496d8
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-06-13 16:10:15 -0700

    Decomposed module into two functions, fixed naming convention for constants and cleaned up comments

--- src/Scripts/email.js
+++ src/Scripts/email.js
@@ -5,23 +5,12 @@
 let nodemailer = require('nodemailer');
 const SMTPTRANSPORT = require('nodemailer/lib/smtp-transport');
 
-/** 
- * Sends email to the passed in email address 
+/**
+ * Sends email to the passed in email address
  * @param {String} email is the email address the user has on file
  */
-function emailUser(email){
-    // Instantiates transporter device to send email with the correct SMTP parameters
-    let transporter = nodemailer.createTransport(new SMTPTRANSPORT({
-        host: 'smtp.ionos.com',
-        port: 587,
-        auth: {
-          user: 'No-Reply@TheCryptocurrencyLedger.com',
-          pass: 'LTwk8wXzq@34'
-        },
-        secure:false,
-            tls: {rejectUnauthorized: false},
-        debug:true
-      }));
+function sendsEmailToUser(email){
+    instantiateTransporter()
 
     // Populate more mailOptions, this will later be made more dynamic
     let mailOptions = {
@@ -29,18 +18,35 @@ function emailUser(email){
         to: email,
         subject: 'Ledger Registration!',
         text: "Welcome to The Cryptocurrency Ledger! \n\tWe're excited to help enhance your trading experience."
-      };
+    };
 
     transporter.sendMail(mailOptions, function(error, info){
         // Debug mode on or off
         if (error) {
-          console.log(error);
+            console.log(error);
         } else {
-          console.log('Email sent: ' + info.response);
+            console.log('Email sent: ' + info.response);
         }
-      }); 
+    });
+}
+
+/**
+ * // Instantiates transporter device to send email with the correct SMTP parameters
+ */
+function instantiateTransporter() {
+    let transporter = nodemailer.createTransport(new SMTPTRANSPORT({
+        host: 'smtp.ionos.com',
+        port: 587,
+        auth: {
+            user: 'No-Reply@TheCryptocurrencyLedger.com',
+            pass: 'LTwk8wXzq@34'
+        },
+        secure:false,
+        tls: {rejectUnauthorized: false},
+        debug:true
+    }));
 }
 
 // Exports this package, so it can be can easily be called from other scripts
-exports.emailUser = emailUser;
+exports.emailUser = sendsEmailToUser;
 
