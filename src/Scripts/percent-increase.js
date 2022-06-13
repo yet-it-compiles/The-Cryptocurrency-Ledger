d5c5e2fa9dbc3f4fedf42d7a8182b244f42aea92
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-06-13 16:12:45 -0700

    Clarified testing section in percent-increase.js

--- src/Scripts/percent-increase.js
+++ src/Scripts/percent-increase.js
@@ -30,7 +30,7 @@ class PercentIncreaseCalculator {
     }
 }
 
-
+//                    ============================ Calculations Testing  ============================
 const calculator = new PercentIncreaseCalculator(0, 1);
 
 console.log("Percent Increase: " + calculator.calculatePercentIncrease())
