370b68d82052f951dc69e80a8852d9b76d8d1af1
Tyler Unsworth <tyler.unsworth@cwu.edu>
2022-06-13 16:16:38 -0700

    Added module header, and removed redundant parantheses around if/else statments.

--- src/Scripts/script.js
+++ src/Scripts/script.js
@@ -1,26 +1,26 @@
+/* Simple module which gives selector functionality to the dashboard */
+
 const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
 
-
-toggle.addEventListener("click" , () =>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click" , () =>{
+searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 })
 
-modeSwitch.addEventListener("click" , () =>{
+modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
+
+    if (body.classList.contains("dark"))
         modeText.innerText = "Light mode";
-    }else{
+    else
         modeText.innerText = "Dark mode";
-        
-    }
+
 });
